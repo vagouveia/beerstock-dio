6a2477eca2aa8a62a4d5c8355bb83f6473f5ee1d
Vitor Alves Gouveia <vitorg@kyros.com.br>
2021-06-25 21:17:05 -0300

    Correção do nome do pacote.

--- src/test/java/one/digitalinnovation/beerstockdio/BeerstockDioApplicationTests.java
+++ src/test/java/one/digitalinnovation/beerstockdio/BeerstockDioApplicationTests.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.personaapi;
+package one.digitalinnovation.beerstockdio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
